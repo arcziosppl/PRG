8c1f84969e88ff4d8c360c625e620cd646425c7f
Artur <70844316+arcziosppl@users.noreply.github.com>
2023-04-02 16:10:37 +0200

    egzamin

---
+++ egzamin/wedkuj.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wędkowanie</title>
+    <link rel="stylesheet" href="styl_1.css">
+</head>
+<body>
+    <header>
+        <div class="baner">
+            <h1>Portal dla Wędkarzy</h1>
+        </div>
+    </header>
+    <main>
+        <div class="lewy1">
+            <h3>Ryby zamieszkujące rzeki</h3>
+        </div>
+        <div class="lewy2">
+            <h3>Ryby drapieżne naszych wód</h3>
+        </div>
+        <div class="prawy">
+            <img src="ryba1.jpg" alt="Sum">
+            <br>
+            <a href="kwerendy.txt">Pobierz kwerendy</a>
+        </div>
+    </main>
+    <footer>
+        <p>Stronę wykonał: 23423423892348</p>
+    </footer>
+</body>
+</html>
